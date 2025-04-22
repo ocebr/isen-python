3c656fd84466df006d076505a5a210220067d920
Malo-35 <malo.barbaud@isen.yncrea.fr>
2025-04-22 14:34:18 +0200

    ci: modification du dockerfile

--- Project/urls.py
+++ Project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('favourite/', include('favourites.urls')),
     path('user/', include('users.urls')),
-    re_path('^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path('^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)/$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)/$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
