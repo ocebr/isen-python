c08b70d2d21afa0ab23b0727d0ef1ea14fd266de
Charlotte <charlotte.montanari@gmail.com>
2025-04-25 14:43:33 +0200

    ci: ajout du job Cypress E2E

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,39 +1,28 @@
 describe('Create and connect to an account', () => {
   it('Visits the Oc commerce site', () => {
-    cy.visit('home')
+    cy.visit('/home');
 
-    // User is able to create an account an to be redirect to login pages
+    const randomId = Math.floor(Math.random() * 100000);
+    const username = `fakeuser${randomId}`;
+    const email = `fake${randomId}@email.com`;
+    const password = '1hstesh<23456789';
 
-    cy.contains('SIGNUP').click()
-    cy.url().should('include', '/user/signup')
-    // cy.contains('fname')
-    cy.get('[id^=fname]').type('fakeuser')
-    cy.get('[id^=lname]').type('toto')
-    cy.get('[id^=username]').type('fakeuser')
-    cy.get('[id^=email]').type('fake@email.com')
-    cy.get('[id^=pass]').type('1hstesh<23456789')
-    cy.get('[id^=re_pass]').type('1hstesh<23456789')
-    cy.get('form').contains('Register').click()
-    cy.url().should('include', '/user/login')
+    cy.contains('SIGNUP').click();
+    cy.url().should('include', '/user/signup');
 
-    // User is able to connect with the previously created account
-    cy.get('[id^=your_name]').type('fakeuser')
-    cy.get('[id^=your_pass]').type('1hstesh<23456789')
-    cy.get('form').contains('Log in').click()
-    cy.url().should('include', '/home')
-    cy.contains('FAVOURITE')
-  })
-})
+    cy.get('[id^=fname]').type('fakeuser');
+    cy.get('[id^=lname]').type('toto');
+    cy.get('[id^=username]').type(username);
+    cy.get('[id^=email]').type(email);
+    cy.get('[id^=pass]').type(password);
+    cy.get('[id^=re_pass]').type(password);
+    cy.get('form').contains('Register').click();
+    cy.url().should('include', '/user/login');
 
-describe('Put item in favourite', () => {
-  it('Connect to OC commerce and put in favourite', () => {
-
-    // In this test you should load the home url and connect with the previous account
-    // You will go to favourite pages to make sure there is no favourite
-    // Then go back to home
-    // You will add an item to favourite
-    // You will go to favourite pages to confirm item is here
-    // You will then delete the item an check it has been successfully deleted
-
-  })
-})
+    cy.get('[id^=your_name]').type(username);
+    cy.get('[id^=your_pass]').type(password);
+    cy.get('form').contains('Log in').click();
+    cy.url().should('include', '/home');
+    cy.contains('FAVOURITE');
+  });
+});
